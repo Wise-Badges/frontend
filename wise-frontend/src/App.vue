7029e1774b838ebc7e46d0995660c4d6a4290128
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-16 11:43:21 +0200

    added footer with placeholders

--- wise-frontend/src/App.vue
+++ wise-frontend/src/App.vue
@@ -3,17 +3,20 @@
   <div id="app">
     <Header />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default ({
   name: 'app',
   components: {
     Header,
+    Footer
   },
 })
 </script>
@@ -25,7 +28,6 @@ export default ({
   font-family: 'Montserratmedium', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: white;
   background-color: #0B1147;
   min-height: 100vh;
--- wise-frontend/src/components/Footer.vue
+++ wise-frontend/src/components/Footer.vue
@@ -1,7 +1,30 @@
 <template>
-  <div id="footer">
+  <footer id="footer">
+    <div class="footer__main">
+      <div class="container">
+        <ul class="social">
+          <li>Twitter</li>
+          <li>Instagram</li>
+          <li>Facebook</li>
+        </ul>
+        <div class="info">
+          <p class="info__site">WISEBadges is a project funded by the Erasmus+ project to improve gender equity in the access to STEM</p>
+          <div class="info__links">
+            <p>Lorem ipsum</p>
+            <p>Contact</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-  </div>
+    <div class="footer__copyright">
+      <div class="container">
+        <p>Copyright if needed</p>
+        <p>Logo's</p>
+
+      </div>
+    </div>
+  </footer>
 </template>
 
 
@@ -13,5 +36,69 @@ export default ({
 
 
 <style lang="scss">
+  $color-dark: #0B1147;
+  $color-purpleDark: #331F9F;
+  $color-purpleLight: #7C6DF1;
+  $color-yellow: #FCDC3A;
+  $color-mint: #A3F7E9;
+  $color-pink: #E598C9;
+  $color-red: #DF7668;
 
+  #footer {
+    /* position: absolute;
+    bottom: 0;
+    width: 100%; */
+    margin-top: 11.2rem;
+  }
+
+  .footer__main {
+    background-color: $color-purpleDark;
+    color: $color-purpleLight;
+
+    & .social {
+      display: flex;
+      justify-content: center;
+      padding: 3.5rem 0;
+      border-bottom: 1px solid $color-purpleLight;
+
+      & li {
+      padding: 0 1rem;
+      }
+    }
+
+    & .info {
+      padding: 3.5rem 0;
+      display: flex;
+      justify-content: space-between;
+
+      & .info__site {
+        max-width: 22rem;
+        line-height: 1.5rem;
+      }
+
+      & .info__links {
+        line-height: 1.5rem;
+        text-align: right;
+        transform: translateX(-1rem);
+
+        & p::after {
+            content: "\02192";
+            transform: translateX(1rem);
+            display: inline-block;
+            vertical-align: top;
+            transition: 0.4s;
+        }
+      }
+    }
+  }
+
+  .footer__copyright {
+    background-color: white;
+    padding: 2rem 0;
+    color: $color-purpleDark;
+    & .container {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 </style>
