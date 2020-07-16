963adea4d419af93e88ae0dafbca685c0de7abb0
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-07-16 15:42:26 +0200

    image stuggle

--- src/components/BadgeSelect.vue
+++ src/components/BadgeSelect.vue
@@ -3,10 +3,10 @@
     <h2>{{ msg }}</h2>
     <ul class="badges-ul">
       <li class="single-badge" id="badgelist" v-for="badge in badges" :key="badge.name">
-        <input class="radio-btn" type="radio" v-model="currentBadge" :id="badge.name" :value="badge.id" name="currentBadge">
+        <input class="radio-btn" type="radio" :checked="checked" v-on:change="$emit('change', $event.target.checked)" v-model="currentBadge" :id="badge.name" :value="badge.id" name="currentBadge">
         <label class="badge-label" :for="badge.name">
-          <img v-bind:class="{activeBadgeHexagon: active}" :src="'assets/img/' + badge.img + '.svg'" v-bind:alt="badge.img">
-          <!-- <img :class="'img-stnd active-badge-' + badge.figure" :src="'assets/img/' + badge.img + '.svg'" v-bind:alt="badge.img"> -->
+          <!-- <img v-bind:class="{activeBadgeHexagon: active}" :src="'assets/img/' + badge.img + '.svg'" v-bind:alt="badge.img"> -->
+          <img :class="'img-stnd active-badge-' + badge.figure" :src="'assets/img/' + badge.img + '.svg'" v-bind:alt="badge.img">
           <p class="badge-name">{{badge.name}}</p>
         </label>
       </li>
@@ -19,6 +19,13 @@
 
 export default ({
   name: 'app',
+  props: {
+    checked: Boolean
+  },
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
   data() {
     return {
       msg: 'Choose a badge to give',
@@ -89,6 +96,9 @@ a {
 
 .radio-btn {
   //display: none;
+  opacity: 0;
+  position: fixed;
+  width: 0;
 }
 
 .badge-name {
@@ -100,13 +110,13 @@ a {
 }
 
 .single-badge {
-  font-family: 'montserratregular';
+  font-weight: 400;
   margin: 2rem;
   opacity: 0.8;
 }
 
 .single-badge:hover {
-  font-family: 'montserratbold';
+  font-weight: 400;
   opacity: 1;
 }
 
@@ -115,7 +125,11 @@ a {
   border: 0.1rem solid transparent;
 }
 
-.active-badge-square {
+.single-badge .active-badge-square {
+  padding: 0.5rem;
+}
+
+.single-badge input[type="radio"]:checked + .active-badge-square {
   padding: 0.5rem;
   border: 0.1rem solid white;
 }
@@ -144,22 +158,12 @@ a {
   background-position-y: -0.1rem;
 }
 
-
-.activeBadgeHexagon {
-  padding: 0.5rem;
-  //border: 0.1rem solid transparent;
-  background-image: url('/assets/img/shapes/hexagon.svg');
-  background-size: 89%;
-  background-repeat: no-repeat;
-  background-position-x: 0.55rem;
-  background-position-y: -0.1rem;
-}
-
+/*
 .badges-ul [type="radio"]:checked + .single-badge {
   background-color: red;
-  font-family: 'montserratbold';
+  font-weight: 700;
   opacity: 1;
   color: orange;
 }
-
+*/
 </style>
