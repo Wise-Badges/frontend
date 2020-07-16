ffb33edc89bc2ba506e27c3af3c4986574367d4d
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-16 11:48:34 +0200

    added content about page

--- wise-frontend/src/views/About.vue
+++ wise-frontend/src/views/About.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <h2>What is a badge?</h2>
-    <p>Lorem ipsum</p>
-    <h2>Why does WISE exist?</h2>
-    <p>Lorem ipsum</p>
+    <div class="container">
+      <h1>About</h1>
+      <p>WiseBadges is created out of the need for recognizing females within the STEM field. A field where females are a minority and struggling to find their ways to be valued, accepted and appreciated. WiseBadges believes it is crucial to recognize the ongoing struggles, aiming to create a positive climate for females studying or working in a STEM domain.</p>
+      <p>When studying at a college or university receiving a diploma and therefore, acknowledgment is a logical step. Times do change, and informal learning is gaining importance. The valuable time devoted to boosting competences needs to be more visible and therefore, more powerful. That is where WiseBadges comes into place, making it possible to validate a specific skill, approach or characteristic.</p>
+      <p>WiseBadges makes the process of giving and receiving recognition easy, fun and inspiring. Awarding people who deserve it, making them feel recognized and especially helping them build their presence within the STEM community. WiseBadges shines a light on the importance of empowering and validating females, aiming to develop a community of like-minded people.</p>
+    </div>
   </div>
 </template>
