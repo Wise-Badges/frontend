34d87069400c0033916e1906b4a22e954dee6495
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-16 11:49:37 +0200

    added line-height in app

--- wise-frontend/src/App.vue
+++ wise-frontend/src/App.vue
@@ -29,6 +29,7 @@ export default ({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: white;
+  line-height: 1.5;
   background-color: #0B1147;
   min-height: 100vh;
   height: 100%;
