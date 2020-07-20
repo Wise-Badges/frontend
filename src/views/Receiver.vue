58379e43a9a4f649e1d1c7532d6773f4ca9f4824
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-07-20 13:38:28 +0200

    css and selected badge ready receiver

--- src/views/Receiver.vue
+++ src/views/Receiver.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="maxi-container">
-    <div>
+    <div class="data-selected-container selectedBadge-container">
       <img :src="'/assets/img/' + this.$store.state.badges[$route.params.id - 1].img + '.svg'" v-bind:alt="this.$store.state.badges[$route.params.id - 1].img">
-      <p>{{this.$store.state.badges[$route.params.id - 1].name}}</p>
+      <p class="selectedBadge-name">{{this.$store.state.badges[$route.params.id - 1].name}}</p>
     </div>
     <h2>Choose who you want to send the badge to</h2>
       <div class="form-container">
@@ -63,4 +63,28 @@ export default {
   outline: 0.1rem solid transparent;
 }
 
+.data-selected-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.selectedBadge-name {
+  letter-spacing: 0.2em;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-left: 2rem;
+  max-width: 14rem;
+}
+
+
+.selectedBadge-container {
+  background-image: url('/assets/img/flag.svg');
+  background-repeat: no-repeat;
+  width: 30rem;
+  height: 7rem;
+  background-position-x: 7rem;
+  margin-bottom: 4rem;
+}
 </style>
