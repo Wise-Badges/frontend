831e980fb81fc8a21b7c94b93356bd0df4f96f55
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-22 11:42:13 +0200

    added styling community page

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="view-badges">
     <h1>Community</h1>
-    <ul class="tweets">
-      <li class="tweet"v-for="tweet in tweets" :key="tweet.id">
-        <p class="tweet__receiver">{{ tweet.receiver }}</p>
-        <p class="tweet__message">{{ tweet.message }}</p>
-        <p class="tweet__issuer">issued by {{ tweet.issuer }} on {{ tweet.platform }}</p>
+    <ul class="acceptedBadges">
+      <li class="acceptedBadge"v-for="badge in badges" :key="badge.id">
+        <p class="badge__receiver">{{ badge.receiver }}</p>
+        <p class="badge__message">{{ badge.message }}</p>
+        <p class="badge__issuer">issued by {{ badge.issuer }} on {{ badge.platform }}</p>
       </li>
     </ul>
   </div>
@@ -17,7 +17,7 @@
     data() {
       return {
         msg: 'Community',
-        tweets: [
+        badges: [
           { id: 1, receiver: 'username', issuer: 'username', message: 'At vero eos et accusamus et iusto odio dignis simos ducimus qui blanditiis praesentium voluptatum  et accusamus et iusto.', date: '24/05/2020', platform: 'Twitter'},
           { id: 2, receiver: 'username', issuer: 'username', message: 'At vero eos et accusamus et iusto odio dignis simos ducimus qui blanditiis praesentium voluptatum  et accusamus et iusto.', date: '24/05/2020', platform: 'Twitter'},
           { id: 3, receiver: 'username', issuer: 'username', message: 'At vero eos et accusamus et iusto odio dignis simos ducimus qui blanditiis praesentium voluptatum  et accusamus et iusto.', date: '24/05/2020', platform: 'Twitter'},
@@ -42,35 +42,49 @@
   $color-pink: #E598C9;
   $color-red: #DF7668;
 
-  .tweets {
+  .acceptedBadges {
     display: grid;
     grid-template: repeat(3, auto) / repeat(2, 1fr);
     grid-gap: 1.875rem;
   }
 
-  .tweet {
+  .acceptedBadge {
     background-color: $color-purpleDark;
     padding: 2rem;
     display: flex;
     flex-direction: column;
 
-    .tweet__receiver {
+    .badge__receiver {
       transform: translateX(2rem);
       margin-bottom: 1rem;
+      font-weight: 700;
+
+      &::before {
+        content: '';
+        position: absolute;
+        background-image: url('/assets/img/award.svg');
+        width: 1.1rem;
+        height: 1.5rem;
+        background-repeat: no-repeat;
+        transform: translate(-2rem, .1rem)
+      }
     }
 
-    .tweet__receiver::before {
-      content: '';
-      position: absolute;
-      background-image: url('/assets/img/award.svg');
-      width: 1.1rem;
-      height: 1.5rem;
-      background-repeat: no-repeat;
-      transform: translate(-2rem, .1rem)
-    }
-
-    .tweet__message {
+    .badge__message {
       margin-bottom: 1rem;
+    }
+
+    .badge__issuer {
+      padding-left: 4rem;
+
+      &::before {
+        content: '';
+        position: absolute;
+        height: .1rem;
+        width: 2rem;
+        background-color: $color-purpleLight;
+        transform: translate(-3rem, .9rem);
+      }
     }
   }
 </style>
