e1495c309cf3036367e64c2b32855922afb91ece
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-26 21:56:49 +0200

    cleanup css files

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from './router';
 import store from './store';
 // Importing the global css file
 import '@/css/global.css';
+import '@/css/typography.css';
+import '@/css/buttons.css';
+import '@/css/form.css';
 
 Vue.config.productionTip = false;
 
