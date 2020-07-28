46d147401ecc8838cf8cd99d2fabc76035f3c967
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-07-28 17:02:55 +0200

    twitter link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="footer__main">
       <div class="container">
         <ul class="social">
-          <li><img alt="Twitter" src="/assets/img/footer/twitter.svg"/><span class="hidden">Twitter</span></li>
+          <li><a href="https://twitter.com/wisebadges" target="_blank"><img alt="Twitter" src="/assets/img/footer/twitter.svg"/></a><span class="hidden">Twitter</span></li>
           <li><img alt="Instagram" src="/assets/img/footer/instagram.svg"/><span class="hidden">Instagram</span></li>
           <li><img alt="Facebook" src="/assets/img/footer/facebook.svg"/><span class="hidden">Facebook</span></li>
         </ul>
