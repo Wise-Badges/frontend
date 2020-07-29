b0bed2555dcc9d6926f9fda3f69b82f0b3d6a310
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-07-29 13:08:18 +0200

    accepted text fixed

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -6,7 +6,7 @@
     <div class="detail">
 
       <div class="detail__info">
-        <p>This badge has not been accepted yet, please accept it by liking the following Tweet. Only @{{this.$store.state.assertionByIdApi.recipient.name}} can officially accept this badge before 21/08/20.</p>
+        <p><span v-if="this.$store.state.assertionByIdApi.accepted === false">This badge has not been accepted yet</span><span v-if="this.$store.state.assertionByIdApi.accepted === true">This badge has not been accepted yet</span>, please accept it by liking the following Tweet. Only @{{this.$store.state.assertionByIdApi.recipient.name}} can officially accept this badge before 21/08/20.</p>
         <div class="tweet__wrapper">
           <iframe class="twitter-answer" :src="'https://platform.twitter.com/embed/index.html?&embedId=twitter-widget-0&hideThread=false&theme=light&id=' + shortenAnswerId()" frameborder="0" height=700 width=500></iframe>
           <iframe class="twitter-answer" :src="'https://platform.twitter.com/embed/index.html?&embedId=twitter-widget-0&hideThread=false&theme=light&id=' + shortenEvidenceId()" frameborder="0" height=300 width=500></iframe>
